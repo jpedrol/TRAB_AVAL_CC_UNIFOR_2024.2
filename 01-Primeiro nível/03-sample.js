b80a7914b0ffa1b3973e4dc8171ebf57b66c04aa
joão pedro lopes Marques De Almeida <joaopedrolopes@edu.unifor.br>
2024-11-12 22:29:56 -0300

    nv-03

--- 01-Primeiro nível/03-sample.js
+++ 01-Primeiro nível/03-sample.js
@@ -1,9 +1,23 @@
-let d1 = "2024-01-01";
-let d2 = "2024-12-31";
-
-function calcDif(d1, d2) {
-    return new Date(d2) - new Date(d1);
+function converterParaData(dataString) {
+    const data = new Date(dataString);
+    if (isNaN(data)) {
+        throw new Error(`Data inválida: ${dataString}`);
+    }
+    return data;
 }
 
-let res = calcDif(d1, d2);
-console.log(res);
+function calcularDiferenca(dataInicial, dataFinal) {
+    const data1 = converterParaData(dataInicial);
+    const data2 = converterParaData(dataFinal);
+    return data2 - data1;
+}
+
+const data1 = "2024-01-01";
+const data2 = "2024-12-31";
+
+try {
+    const resultado = calcularDiferenca(data1, data2);
+    console.log(resultado);
+} catch (error) {
+    console.error("Erro:", error.message);
+}
