c68a05a3e1b5e9b62937c70ef44f4683822364f6
joão pedro lopes Marques De Almeida <cristian.13@edu.unifor.br>
2024-11-13 00:24:25 -0300

    n45-402-verdadeiro

--- 04-Quarto nível/02-sample.js
+++ 04-Quarto nível/02-sample.js
@@ -1,8 +1,17 @@
 async function buscarFilme() {
-    const resposta = await fetch("https://swapi.dev/api/films/1/");
-    const filme = await resposta.json();
+    try {
+        const resposta = await fetch("https://swapi.dev/api/films/1/");
+        
+        if (!resposta.ok) {
+            throw new Error("Erro ao buscar o filme.");
+        }
 
-    console.log(filme.titulo.toUpperCase());
+        const filme = await resposta.json();
+        console.log(filme.title.toUpperCase()); // "title" é o campo correto, não "titulo"
+
+    } catch (erro) {
+        console.error("Erro:", erro.message);
+    }
 }
 
 buscarFilme();
