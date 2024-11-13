df39cd350055ed33fbf3b3c060d480fb65ae1408
joão pedro lopes Marques De Almeida <joaopedrolopes@edu.unifor.br>
2024-11-12 23:46:33 -0300

    nv45-404

--- 04-Quarto nível/04-sample.js
+++ 04-Quarto nível/04-sample.js
@@ -1,6 +1,11 @@
 async function buscarPersonagem(id) {
-    const url = encodeURIComponent(`https://swapi.dev/api/people/${id}/%%%`);
+    const url = `https://swapi.dev/api/people/${id}/`;
     const resposta = await fetch(url);
+    
+    if (!resposta.ok) {
+        throw new Error("Personagem não encontrado.");
+    }
+    
     const personagem = await resposta.json();
     console.log(personagem.name);
 }
