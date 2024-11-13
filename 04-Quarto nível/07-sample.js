f340b567b9cdce0aac364321137039fa5d73c7c4
joão pedro lopes Marques De Almeida <cristian.13@edu.unifor.br>
2024-11-13 00:26:27 -0300

    n45-407-verdadeiro

--- 04-Quarto nível/07-sample.js
+++ 04-Quarto nível/07-sample.js
@@ -1,8 +1,21 @@
 async function buscarEspecie() {
-    const resposta = await fetch("https://swapi.dev/api/species/1/");
-    const especie = await resposta.json();
+    try {
+        const resposta = await fetch("https://swapi.dev/api/species/1/");
+    
+        if (!resposta.ok) {
+            throw new Error("Espécie não encontrada.");
+        }
 
-    console.log(especie.average_lifespan.toUpperCase());
+        const especie = await resposta.json();
+        
+        if (especie.average_lifespan) {
+            console.log(`A expectativa de vida média é ${especie.average_lifespan} anos.`);
+        } else {
+            console.log("A expectativa de vida média não está disponível.");
+        }
+    } catch (erro) {
+        console.error("Erro:", erro.message);
+    }
 }
 
 buscarEspecie();
