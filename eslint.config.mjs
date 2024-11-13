9ef70cef1675323021099bffb3246e8cec6f2e06
joão pedro lopes Marques De Almeida <joaopedrolopes@edu.unifor.br>
2024-11-13 00:15:33 -0300

    configs-basicas

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "eol-last": "error",
+      indent: [
+        "error",
+        4,
+        {
+          SwitchCase: 1,
+        },
+      ],
+      "linebreak-style": "off",
+      "max-len": ["error", 120],
+      "max-lines-per-function": ["warn", 30],
+      "object-curly-spacing": ["error", "always"],
+      quotes: ["error", "double"],
+      "quote-props": ["error", "as-needed"],
+      semi: ["error", "always"],
+      "no-magic-numbers": ["error", { ignore: [0, 1] }],
+      "consistent-return": "error",
+      "max-lines": [
+        "error",
+        { max: 300, skipBlankLines: true, skipComments: true },
+      ],
+      complexity: ["error", 5],
+      "max-params": ["error", 4],
+      "no-duplicate-imports": "error",
+      "prefer-const": "error",
+      "no-unused-expressions": [
+        "error",
+        { allowShortCircuit: true, allowTernary: true },
+      ],
+      "prefer-template": "error",
+    },
+    ignores: ["**/node_modules/**"],
+}
+];
