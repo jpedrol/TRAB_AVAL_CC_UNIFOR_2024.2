ab016d274f7758bffd517d8c412e738e460f8dd5
joão pedro lopes Marques De Almeida <joaopedrolopes@edu.unifor.br>
2024-11-12 22:30:34 -0300

    nv-04

--- 01-Primeiro nível/04-sample.js
+++ 01-Primeiro nível/04-sample.js
@@ -1,6 +1,20 @@
-let fcr = function(a, b) {
-    return a + b;
-}
+const primeiroNumero = 3;
+const segundoNumero = 5;
 
-let rsl = fcr(3, 5);
-console.log(rsl);
+function validarNumero(numero) {
+    if (typeof numero !== "number") {
+        throw new Error(`${numero} não é um número válido`);
+    }
+    return numero;
+}
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+try {
+    const numero1Valido = validarNumero(primeiroNumero);
+    const numero2Valido = validarNumero(segundoNumero);
+    const resultado = somar(numero1Valido, numero2Valido);
+    console.log(resultado);
+} catch (erro) {
+    console.error("Ocorreu um erro:", erro.message);
+}
