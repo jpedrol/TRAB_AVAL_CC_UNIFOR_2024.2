d27bc94b44cf9f2d3629af645d56f2afd1e42a46
joão pedro lopes Marques De Almeida <joaopedrolopes@edu.unifor.br>
2024-11-12 22:30:55 -0300

    nv-05

--- 01-Primeiro nível/05-sample.js
+++ 01-Primeiro nível/05-sample.js
@@ -1,11 +1,20 @@
-let a = "João";
-let b = "Ana";
-let c = "Carlos";
+const nome1 = "João";
+const nome2 = "Ana";
+const nome3 = "Carlos";
 
-function p() {
-    console.log(a);
-    console.log(b);
-    console.log(c);
+function exibirNome(nome) {
+    if (typeof nome !== "string") {
+        throw new Error("Nome inválido");
+    }
+    console.log(nome);
 }
-
-p();
+function exibirTodosOsNomes() {
+    try {
+        exibirNome(nome1);
+        exibirNome(nome2);
+        exibirNome(nome3);
+    } catch (erro) {
+        console.error("Erro ao exibir nome:", erro.message);
+    }
+}
+exibirTodosOsNomes();
